1d77de742502379cd910c010db1d9507f60c6b27
Garrett Tolbert <garrett1.tolbert@gmail.com>
2021-02-03 10:44:22 -0600

    updated index page and added public route

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,29 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useAuth } from '../hooks/useAuth';
+import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	const { user } = useAuth();
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+	const signout = async () => {
+		await firebase.auth().signOut();
+		router.push('/');
+	};
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+	return (
+		<>
+			<div>{user ? user.uid : 'No user signed in'}</div>
+			<Link href="/public">
+				<button>Go to public page</button>
+			</Link>
+			<Link href="/private">
+				<button>Go to private page</button>
+			</Link>
+			{!user && (
+				<Link href="/login">
+					<button>login</button>
+				</Link>
+			)}
+			{user && <button onClick={signout}>Signout</button>}
+		</>
+	);
 }
---
+++ pages/public.js
@@ -0,0 +1,3 @@
+export default function Public() {
+	return <div>This is a public page. No authentication needed </div>;
+}
